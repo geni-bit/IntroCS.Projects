c73f2cc2d09f947482a55e6cab37dafcb99684a8
geni-bit <gjhernandezsosa@gmail.com>
2025-07-09 22:35:09 -0700

    Completed heptagon.py

---
+++ Project 2/heptagon.py
@@ -0,0 +1,14 @@
+# IntroCS - Taksk #1
+
+# The program below uses the turtle module to draw a heptagon
+
+# import turtle module
+import turtle
+
+# create junior the turtle
+junior = turtle.Turtle()
+
+# junior creating a heptagon
+for i in range(7):
+    junior.forward(50)
+    junior.left(51.4)
